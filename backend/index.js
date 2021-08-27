2d2df417140d8f6afba9b6c207179e331273eff3
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2021-08-27 21:45:00 +0530

    getAllProducts function done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
-import  express  from "express";
+import express from "express";
+import db from "./config/database.js";
+//Page routing 
+import productRoutes from "./routes/index.js";
+import cors from 'cors';
 
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('welcome');
-});
 
-app.listen(6000, () => console.log('Server running ar port 6000'));
+try {
+
+    await db.authenticate();
+    console.log('Database connected ... ');
+
+}catch (error){
+
+    console.error('Connection Error:', error);
+}
+
+app.use(cors());
+app.use(express.json());
+app.use('/products',productRoutes);
+
+app.listen(5000, () => console.log('Server running ar port 5000'));
---
+++ backend/routes/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+import  { 
+    getAllProducts
+} from '../controllers/Products.js'
+
+const router = express.Router();
+router.get('/',getAllProducts)
+export default router;
