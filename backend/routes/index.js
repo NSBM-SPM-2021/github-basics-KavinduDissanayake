cd91a0159c9ce2e0e709cae53791e51d2fe90e47
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2021-08-27 22:09:41 +0530

    UPDATE API TEST

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,12 +3,14 @@ import express from "express";
 import  { 
     getAllProducts,
     createProduct,
-    getProductById
+    getProductById,
+    updateProduct
 } from '../controllers/Products.js'
 
 const router = express.Router();
 router.get('/',getAllProducts)
 router.post('/',createProduct)
 router.get('/:id',getProductById)
+router.patch('/:id',updateProduct)
 
 export default router;
