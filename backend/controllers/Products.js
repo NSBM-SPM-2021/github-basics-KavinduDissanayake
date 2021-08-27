6762f98b28eeada9ea710219dee5377f6e6beb4b
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2021-08-27 21:48:02 +0530

    createProduct function created

--- backend/controllers/Products.js
+++ backend/controllers/Products.js
@@ -1,8 +1,6 @@
 import Product  from "../Models/productsModel.js";
 
 export const getAllProducts = async (req, res) => {
-
-
     try {
         const products = await Product.findAll();
         res.json(products);
@@ -11,6 +9,21 @@ export const getAllProducts = async (req, res) => {
 
         res.json({ message: error.message });
     }
+}
 
+
+//CREATE PRODUCT FUNCTION 
+export const createProduct = async (req, res) => {
+    try {
+        const products = await Product.create(req.body);
+        res.json({
+            "message": "Product Created"
+        });
+
+    }catch (error){
+
+        res.json({ message: error.message });
+    }
 
 }
+
