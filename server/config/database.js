0fba32d8ba976e5930d475e61453f6c5883c18f5
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2021-09-05 23:36:49 +0530

    Datbase Config with Server

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
-//Datbase Config 
-const db = new Sequelize('spm_db', 'root', '', {
-    host: "localhost",
+//Datbase Config           datbase name             username         password
+const db = new Sequelize('heroku_5f5edbc8dc00cca', 'b664fbdfa94763', 'd59a8f44', {
+    host: "us-cdbr-east-04.cleardb.com",
     dialect: "mysql"
 
 });
