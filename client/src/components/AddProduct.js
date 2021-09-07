7083e60bf4662603fa59b609613aadb3e4b72802
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2021-09-07 14:33:10 +0530

    AddProduct view add

---
+++ client/src/components/AddProduct.js
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+export const AddProduct = () => {
+
+    const [title, setTitle] = useState('');
+    const [price, setPrice] = useState('');
+
+    const history = useHistory();
+
+    //SAVE PRODUCT
+
+    const saveProduct = async (e) => {
+        e.preventDefault();
+
+        await axios.post('http://localhost:5000/products', {
+            title: title,
+            price: price
+        });
+
+        history.push("/");
+    }
+
+    return (
+        <div className="container text-center" style={{ display: "inline-flex", justifyContent: "center"}}>
+            <div class="card" style={{ width: "30rem" }}>
+                    <div class="card-body" >
+                        <h5 class="card-title" style={{ color: "grey" }}>Add Products</h5>
+                        <p class="card-text">
+                            <form onSubmit={saveProduct}>
+                               
+                                <div className="field">
+                                    <input
+                                        class="form-control"
+                                        type="text"
+                                        placeholder="Title"
+                                        value={title}
+                                        onChange={(e) => setTitle(e.target.value)}
+                                    />
+                                </div>
+                                <p></p>
+                                <div className="field">
+                                    <input
+                                        class="form-control"
+                                        type="text"
+                                        placeholder="Price"
+                                        value={price}
+                                        onChange={(e) => setPrice(e.target.value)}
+                                    />
+                                </div>
+                                <button className="btn btn-primary mx-0 my-3">Save</button>
+
+                            </form>
+                        </p>
+                    </div>
+             </div>
+        </div>
+    )
+}
