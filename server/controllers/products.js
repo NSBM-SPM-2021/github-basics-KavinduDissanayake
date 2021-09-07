a8e99e71d0f0f9c5d1b0148be25d4330163cfcdf
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2021-09-07 15:08:55 +0530

    create api for get products

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -22,8 +22,6 @@ export const createProduct = async (req, res) => {
 
 //API FOR getAllProducts
 export const getAllProducts = async (req, res) => {
-
-
     try {
         const products = await Product.findAll();
         res.json(products);
@@ -32,6 +30,4 @@ export const getAllProducts = async (req, res) => {
 
         res.json({ message: error.message });
     }
-
-
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 
 
 import  { 
+    getAllProducts ,
     createProduct
 } 
 from '../controllers/products.js'
@@ -10,6 +11,8 @@ from '../controllers/products.js'
 
 const router = express.Router();
 
+//API ROUTES
+router.get('/',getAllProducts)
 router.post('/',createProduct)
 
 export default router;
