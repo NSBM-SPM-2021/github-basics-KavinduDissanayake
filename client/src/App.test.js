85195bb0dc72acc2ad377b6a79c317e660ed38e8
Kavindu Dissanayake <51842810+KavinduDissanayake@users.noreply.github.com>
2021-10-15 16:00:57 +0530

    show deployment

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -23,25 +23,5 @@ describe("AddProduct attribute Test", () => {
 });
 
 
-describe("EditProduct attribute Test", () => {
-
-  test('Renders Title  filed', () => {
-    render(<EditProduct />);
-    const title = screen.getByTestId("adTitle");
-    expect(title).toBeInTheDocument();
-    expect(title).toHaveAttribute("type", "text");
-  });
-
-
-  test('Renders Price filed', () => {
-    render(<EditProduct />);
-    const price = screen.getByTestId("adPrice");
-    expect(price).toBeInTheDocument();
-    expect(price).toHaveAttribute("type", "text");
-  });
-
-});
-
-
 
 
